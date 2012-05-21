9f42450bbfa4b150d2f555d4bb97b6a86ac97938
Stefano Balietti <futur.dorko@gmail.com>
2012-05-21 09:59:59 +0200

    added DONE buttons in frame

--- pr.client.js
+++ pr.client.js
@@ -274,8 +274,8 @@ function PeerReviewGame () {
 						}
 					}
 
-					$('#mainframe').contents().find('#done_box').before(table.parse());
-					//node.window.getElementById('container_exhibition').appendChild(table.parse());
+					//$('#mainframe').contents().find('#done_box').before(table.parse());
+					node.window.getElementById('container_exhibition').appendChild(table.parse());
 					this.all_ex.addDD(table);
 
 				}
