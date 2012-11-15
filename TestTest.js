84b22d4b6b4a3c46cddbff4a53dc9b59f9f6054b
Stefano Balietti <futur.dorko@gmail.com>
2012-11-15 22:23:27 +0100

    mods

--- TestTest.js
+++ TestTest.js
@@ -12,7 +12,7 @@ window.onload = function() {
 //s.appendChild(b);
 //document.body.appendChild(s)
 
-function format_string(string, args, root) {
+function sprintf(string, args, root) {
 		
 	var text, textNode, span, idx_start, idx_finish, idx_replace, idxs, spans = {};
 	
@@ -26,7 +26,7 @@ function format_string(string, args, root) {
 	for (var key in args) {
 		if (args.hasOwnProperty(key)) {
 			
-			idx_start = string.indexOf(key);
+			
 
 			// pattern not found
 			if (idx_start === -1) continue;
@@ -34,7 +34,8 @@ function format_string(string, args, root) {
 			switch(key[0]) {
 			
 			case '%': // span
-			
+				
+				idx_start = string.indexOf(key);
 				idx_replace = idx_start + key.length;
 				idx_finish = string.indexOf(key, idx_replace);
 				
@@ -42,22 +43,12 @@ function format_string(string, args, root) {
 					console.log('Error. Could not find closing key: ' + key);
 					continue;
 				}
-
-				span = W.getElement('span', null, args[key]);
-
-				spans[idx_start] = key;
 				
-				spans[idx_start] = {
-						span: span,
-						idx_finish: idx_finish,
-						idx_replace: idx_replace,
-						marker_length: key.length,
-				};
+				spans[idx_start] = key;
 				
 				break;
 			
 			case '@': // replace and sanitize
-				
 				string = string.replace(key, escape(args[key]));
 				break;
 				
@@ -72,69 +63,55 @@ function format_string(string, args, root) {
 		}
 	}
 	
-	// No substitution was made
+	// No span to creates
 	if (!JSUS.size(spans)) {
 		return document.createTextNode(string);
 	}
 	
 	// Re-assamble the string
 	
-	idxs = JSUS.keys(spans).sort(function(a,b){return a-b});
-	
-	console.log('BEFORE SPAN')
-	
-	console.log()
-	
-	idx_start = -1;
+	idxs = JSUS.keys(spans).sort(function(a,b){return a-b;});
+	idx_finish = 0;
 	for (var i = 0; i < idxs.length; i++) {
-		
-		// add fragments of string
-		if (idx_start !== idxs[i]-1) {
-			root.appendChild(document.createTextNode(string.substring(idx_start, idxs[i])));
-		}
 		
 		// add span
 		key = spans[idxs[i]];
+		idx_start = string.indexOf(key);
+		
+		// add fragments of string
+		if (idx_finish !== idx_start-1) {
+			root.appendChild(document.createTextNode(string.substring(idx_finish, idx_start)));
+		}
 		
 		idx_replace = idx_start + key.length;
 		idx_finish = string.indexOf(key, idx_replace);
 		
-		if (idx_finish === -1) {
-			console.log('Error. Could not find closing key: ' + key);
-			continue;
-		}
-
 		span = W.getElement('span', null, args[key]);
 
-		spans[idx_start] = key;
+		text = string.substring(idx_replace, idx_finish);
 		
-		spans[idx_start] = {
-				span: span,
-				idx_finish: idx_finish,
-				idx_replace: idx_replace,
-				marker_length: key.length,
-		};
+		span.appendChild(document.createTextNode(text));
 		
-		
-		text = string.substring(spanInfo.idx_replace, spanInfo.idx_finish);
-		
-		spanInfo.span.appendChild(document.createTextNode(text));
-		
-		root.appendChild(spanInfo.span);
-		idx_start = spanInfo.idx_finish + spanInfo.marker_length;
-		//idx_start = spans[idxs[i]].finish + spans[idxs[i]].marker_length;
+		root.appendChild(span);
+		idx_finish = idx_finish + key.length;
 	}
 	
 	// add the final part of the string
-	if (idx_start !== string.length) {
-		root.appendChild(document.createTextNode(string.substring(idx_start)));
+	if (idx_finish !== string.length) {
+		root.appendChild(document.createTextNode(string.substring(idx_finish)));
 	}
 	
 	return root;
 }
+	
+	
 //
 //
-var tn = format_string('%s!xxx%s, adesso vai a %ucasa%u!!%nono no non ci vado!%nono.', {
+	
+	var s = '%s!xxx%s, adesso vai a %ucasa%u!!%nono no non ci vado!%nono.';
+	var s = '%s!xxx %s, adesso vai a %ucasa%u!!%nono no non ci vado!%nono.'
+	
+var tn = sprintf(, {
 	'%nono': {
 		id: 'nono',
 	},
@@ -145,10 +122,10 @@ var tn = format_string('%s!xxx%s, adesso vai a %ucasa%u!!%nono no non ci vado!%n
 		id: 'mamma',
 		style: 'font-size: 20px; font-family: courier;',
 	},
-	'!xxx': 'Bravo',
+	'!xxx': '<strong>Bravo<strong>',
 }, document.body);
 
-var s = '%s!xxx%s, adesso vai a %ucasa%u!!%nono no non ci vado!%nono.';
+
 console.log(s.replace('!xxx', 'Bravo'));
 
 //console.log(tn);
